eb8ad9e596b20132d4b867d98a78e1b1e5e92ffd
parkkaka <fdfd54312@naver.com>
2022-05-21 17:46:11 +0900

    Updates

---
+++ precache-manifest.1688ffc05fde247b95e7c305b00b8ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557064a4a28bcf006e0e3a7d7d7c3daa",
+    "url": "/Second-Portfolio-Clone-Coding-react/index.html"
+  },
+  {
+    "revision": "348cef03b398f976e91c",
+    "url": "/Second-Portfolio-Clone-Coding-react/static/css/main.dd66e9dc.chunk.css"
+  },
+  {
+    "revision": "293f2d0234843f59bdf0",
+    "url": "/Second-Portfolio-Clone-Coding-react/static/js/2.59b0d8aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Second-Portfolio-Clone-Coding-react/static/js/2.59b0d8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348cef03b398f976e91c",
+    "url": "/Second-Portfolio-Clone-Coding-react/static/js/main.0ff0d4b4.chunk.js"
+  },
+  {
+    "revision": "21448e9525a0af2ec592",
+    "url": "/Second-Portfolio-Clone-Coding-react/static/js/runtime-main.ee4089dd.js"
+  }
+]);
